291410ef49e02a9b93d60c5324ae3a2bba12d4ae
nosby2007 <lunamelika2018@gmail.com>
2024-09-01 00:19:49 -0400

    video

--- src/app/service/oder-details.service.ts
+++ src/app/service/oder-details.service.ts
@@ -20,49 +20,53 @@ export class OderDetailsService {
       "clothesDescription": "6 Months to become a Devops",
       "category": "men's clothing",
       "clothesImg": "https://res.cloudinary.com/dtdpx59sc/image/upload/v1722757353/Devops1_pg6wx0.jpg",
+      "video": "https://www.youtube-nocookie.com/embed/vBOAgYJovoM?si=wbBZoi4yc5xUx6s4",
       "rating": {
           "rate": 3.9,
           "count": 120
       }
   },
     {
-      "id": 1,
+      "id": 2,
       "clothesName": "Cyber Security",
       "clothesPrice": 250,
       "clothesDescription": " excel in the fast-paced world of DevOps.",
       "category": "IT",
       "clothesImg": "https://res.cloudinary.com/dtdpx59sc/image/upload/v1722757578/cyber_secu_iuzal5.jpg",
+      "video": "https://youtu.be/qRWMDl5f5sA?si=q6pXK99AWKBeidSn",
       "rating": {
           "rate": 3.9,
           "count": 120
       }
   },
     {
-      "id": 1,
+      "id": 3,
       "clothesName": "CLOUD COMPUTING",
       "clothesPrice": 250,
       "clothesDescription": "Architecture Cloud Native comme un Pro ",
       "category": "IT",
       "clothesImg": "https://res.cloudinary.com/dtdpx59sc/image/upload/v1722757577/cloud_computing_jiskn1.png",
+      "video": "https://youtu.be/h7la6rSfgxQ?si=JeRaJww1hd9vjBJR",
       "rating": {
           "rate": 3.9,
           "count": 120
       }
   },
     {
-      "id": 1,
+      "id": 4,
       "clothesName": "DEVELOPPER WEB",
       "clothesPrice": 250,
       "clothesDescription": "Our DevOps courses are designed to you.",
       "category": "IT",
       "clothesImg": "https://res.cloudinary.com/dtdpx59sc/image/upload/v1722757577/Website-Development_syzev9.webp",
+      "video": "https://www.youtube-nocookie.com/embed/vBOAgYJovoM?si=wbBZoi4yc5xUx6s4",
       "rating": {
           "rate": 3.9,
           "count": 120
       }
   },
     {
-      "id": 1,
+      "id": 5,
       "clothesName": "DEVELOPPER WEB",
       "clothesPrice": 250,
       "clothesDescription": "Our DevOps courses are designed to you.",
@@ -74,7 +78,7 @@ export class OderDetailsService {
       }
   },
     {
-      "id": 1,
+      "id": 6,
       "clothesName": "DEVELOPPER WEB",
       "clothesPrice": 250,
       "clothesDescription": "Our DevOps courses are designed to you.",
@@ -86,7 +90,7 @@ export class OderDetailsService {
       }
   },
     {
-      "id": 1,
+      "id": 7,
       "clothesName": "DEVELOPPER WEB",
       "clothesPrice": 250,
       "clothesDescription": "Our DevOps courses are designed to you.",
@@ -98,7 +102,7 @@ export class OderDetailsService {
       }
   },
     {
-      "id": 1,
+      "id": 8,
       "clothesName": "DEVELOPPER WEB",
       "clothesPrice": 250,
       "clothesDescription": "Our DevOps courses are designed to you.",
