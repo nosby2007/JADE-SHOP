b57c5996ced46f06f666776bb378a3dbf90f33b4
nosby2007 <lunamelika2018@gmail.com>
2024-11-10 01:06:01 -0500

    new release

--- src/app/pages/add-product/add-product.component.ts
+++ src/app/pages/add-product/add-product.component.ts
@@ -1,4 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore,AngularFirestoreCollection,AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { Observable, observable } from 'rxjs';
+import { NgPipesModule } from 'ng-pipes';
+import { Action } from 'rxjs/internal/scheduler/Action';
+interface Post {
+  id: number;
+  category: string
+  nom: string;
+  prix: number;
+  description: string;
+  image: string;
+  rating:number;
+  }
+  interface PostId extends Post{
+    id:number
+  }
+  
 
 @Component({
   selector: 'app-add-product',
@@ -6,10 +23,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent implements OnInit {
+  postsCol!: AngularFirestoreCollection<Post>;
+  posts!: any;
 
-  constructor() { }
+  id!: number;
+  category!: string
+  nom!: string;
+  prix!: number;
+  description!: string;
+  image!: string;
+  rating!:number;
 
-  ngOnInit(): void {
+  constructor(private afs:AngularFirestore){}
+
+  ngOnInit(){
+    this.postsCol =this.afs.collection('produit');
+    this.posts = this.postsCol.valueChanges();
+  }
+  addPost(){
+    this.afs.collection('produit').add(
+      {
+        
+  id:this.id,
+  category: this.category,
+  nom: this.nom,
+  prix: this.prix,
+  description: this.description,
+  image:this.image ,
+  rating: this.rating,
+      }
+    )
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { OderDetailsService } from 'src/app/service/oder-details.service';
+import { AngularFirestore,AngularFirestoreCollection,AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { Observable, observable } from 'rxjs';
+import { NgPipesModule } from 'ng-pipes';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
+interface Post {
+  id: number;
+  category: string
+  nom: string;
+  prix: number;
+  description: string;
+  image: string;
+  rating:number;
+  }
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,16 +21,20 @@ import { OderDetailsService } from 'src/app/service/oder-details.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private service:OderDetailsService){}
-  
+  constructor(private service:OderDetailsService, private afs:AngularFirestore){}
+  postsCol!: AngularFirestoreCollection<Post>;
+  posts!: any;
+
   clothesData: any;
 
 
   ngOnInit(): void {
     this.clothesData = this.service.clothesDetails;
-
+    this.postsCol =this.afs.collection('produit');
+    this.posts = this.postsCol.valueChanges();
 
   };
 
+  
   
 }
